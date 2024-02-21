75288d5d78a28d0a29ff04b100d7c8da1d97770a
phonkd <enst18.12@gmail.com>
2024-02-21 20:32:59 +0100

    kekl

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Mini-va Nullnummer & Phonkd",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
